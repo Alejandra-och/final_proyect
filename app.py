fa373c55f29c8521ea3ef38a666e23739afe951f
Alejandra-ochoa <alejaochoa45@gmail.com>
2025-09-04 09:33:13 -0500

    La aplicación permite visualizar los datos de la tabla y cuenta con dos botones para   crear un histograma y un diagrama de dispersión.

---
+++ app.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
+
+st.header("Visualización de datos de anuncios de venta de Vehículos en EE.UU.")
+# Mostrar tabla de datos
+st.dataframe(car_data)
+
+hist_button = st.button('Construir histograma')  # crear un botón
+
+
+if hist_button:  # al hacer clic en el botón
+    # escribir un mensaje
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer",
+                       title='Cantidad de vehiculos según su kilometraje(millas)')
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+
+scatter_button = st.button(
+    'Construir diagrama de dispersión')  # crear un botón
+
+
+if scatter_button:  # al hacer clic en el botón
+    # escribir un mensaje
+    st.write(
+        'Creación de un grafico de dispersión para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un gráfico de dispersión
+    fig = px.scatter(car_data, x="odometer", y="price",
+                     title='Relación entre el precio y el kilometraje(millas)')
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
